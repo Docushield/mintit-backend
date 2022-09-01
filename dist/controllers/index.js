afaab93fbf18da0f884586faf8f55c65f059710b
Ag <aravindmallapureddy@juspay.in>
2022-09-01 19:47:12 +0530

    removed dist folder

--- dist/controllers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logout = exports.login = void 0;
-const Login_1 = require("./Login/Login");
-Object.defineProperty(exports, "login", { enumerable: true, get: function () { return Login_1.login; } });
-Object.defineProperty(exports, "logout", { enumerable: true, get: function () { return Login_1.logout; } });
