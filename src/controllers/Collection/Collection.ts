325f38ca4eb8f11a485ef79bc3f54dd59cd1ff18
ACreed <aravindgopal.ag@gmail.com>
2022-09-09 18:17:37 +0530

    [WIP] Add NFT-INIT-COLLECTION (#2)
    * Added logic for nft creation on testnet.
    
    * Fixed the meta info missing.
    
    * refactored.
    
    * Reindent.
    
    * Working collections API.
    
    * Changed the NFT function.
    
    * Changed the keys to be env.
    
    * Moved the valid token to common function.
    
    * Removed unwanted console logs.
    
    Co-authored-by: Ag <aravindmallapureddy@juspay.in>

--- src/controllers/Collection/Collection.ts
+++ src/controllers/Collection/Collection.ts
@@ -5,6 +5,8 @@ import { AuthTokenRepository } from '../../repository/authtoken';
 import { CollectionRepository } from '../../repository/collection';
 import { Collection } from '../../models/collection';
 import { TokenStatus } from '../../models/authtoken';
+import { lang, crypto } from 'pact-lang-api';
+import Pact from 'pact-lang-api';
 
 export class CollectionController {
     private authTokenRespository: AuthTokenRepository;
@@ -22,18 +24,38 @@ export class CollectionController {
     }
 
 
-    addCollection(req: Request, res: Response) {
-        console.log(req.headers['x-auth-token']);
-        // FIXME: add validation on the above header
-        this.findToken(req.header['x-auth-token']).then(d => {if(Object.keys(d).length == 0)
-                                                                res.status(400).send({error: "Token Not found"});
-                                                              else if (d["status"] == TokenStatus.DISABLED)
-                                                                res.status(400).send({error: "Token is Disabled"});
-                                                              else {
-                                                                  this.collectionRepository.createCollection(req.body);
-                                                                  // FIXME: Convert the JSON to a smart contract call (free.mintit-policy.init-nft-collection ...) and sign using app private keys
-                                                                  res.status(200).json({});
-                                                              }
-                                                             });
+    async addCollection(req: Request, res: Response) {
+        let tokenU = req.headers['x-auth-token'];
+        let d = await this.authTokenRespository.validateToken(tokenU, res);
+        if(d != null){
+          this.collectionRepository.createCollection(req.body);
+          let tokenListHashes = req.body["token-list"].map(val => {
+            return val.hash;
+          });
+          let expression = `(free.z74plc.init-nft-collection {"creator": ${JSON.stringify(req.body.creator)}, "description": ${JSON.stringify(req.body.description)}, "name" : ${JSON.stringify(req.body.name)}, "type": ${JSON.stringify(req.body.type)}, "provenance-hash": ${JSON.stringify(req.body["provenance-hash"])}, "mint-starts": (time ${JSON.stringify(req.body["mint-starts"])}), "premint-ends": (time ${JSON.stringify(req.body["premint-ends"])}), "premint-whitelist": ${JSON.stringify(req.body["premint-whitelist"])}, "size": ${req.body.size}, "mint-price": ${req.body["mint-price"].toFixed(2)}, "sale-royalties": ${JSON.stringify(req.body["sale-royalties"])}, "mint-royalties": ${JSON.stringify(req.body["mint-royalties"])}, "fungible": coin, "token-list": ${JSON.stringify(tokenListHashes)}})`;
+          console.log(expression);
+          let kp =  { publicKey: process.env.PUBLIC_KEY
+                    , secretKey: process.env.SECRET_KEY
+                    };
+          let api_host = process.env.API_HOST || "https://api.testnet.chainweb.com";
+          let networkId = process.env.NETWORK_ID || "testnet04";
+          let chainId = process.env.CHAIN_ID || "1";
+          let metaInfo = lang.mkMeta("k:" + kp.publicKey, chainId, 0.0001, 1000, Math.floor(new Date().getTime() / 1000), 28800);
+          let cmd = [{ keyPairs: kp
+                      , pactCode: expression
+                      , meta: metaInfo
+                      , networkId: networkId
+                      }];
+
+          Pact.fetch.send(cmd, api_host + "/chainweb/0.0/" + networkId + "/chain/" + chainId + "/pact")
+              .then(d => { console.log("data receieve", d);
+                          res.status(200).json({response: d});
+                        }
+                  )
+              .catch(e => { console.log("error", e);
+                            res.status(500).json({error: e});
+                          }
+                    );
+       }
     }
 }
